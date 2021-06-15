c5a1bece06d9f3a4cd547b95238f33b8fcb49ae1
Pin-Yen Huang <pyhuang97@gmail.com>
2021-06-15 19:02:21 +0800

    change data format.

--- dataset.py
+++ dataset.py
@@ -11,8 +11,8 @@ with open('config.yaml', 'r') as stream:
     config = yaml.safe_load(stream)
 
 
-def process_wiki():    
-    with open(config['wiki_out'], 'r') as f:
+def process_wiki(fname):    
+    with open(fname, 'r') as f:
         wiki = json.load(f)    
     
     for datum in tqdm(wiki.values()):
@@ -27,27 +27,31 @@ def process_wiki():
     return wiki
 
 
-def process_trainjsonl():
-    with open(config['train_data'], "r", encoding="utf-8") as f:
+def process_trainjsonl(fname):
+    with open(fname, "r", encoding="utf-8") as f:
         json_strs = f.readlines()
     
     out = []
 
     for json_str in json_strs:
         dic = json.loads(json_str)
+        _id = dic['id']
         claim = dic['claim']
         label = dic['label']
         
-        clean_evidences = set()
+        # One sample of clean_evidence
+        # {doc_id: [sent_id, sent_id, ...]}
+        clean_evidences = {}
         for evidences in dic["evidence"]:
             for evidence in evidences:
                 if(evidence[2] is not None):
                     doc_id = normalize("NFKD", evidence[2])
                     sent_id = evidence[3]
-                    clean_evidences.add((doc_id, sent_id))
+                    clean_evidences[doc_id] = clean_evidences.get(doc_id, []) + [sent_id]
         
         out.append(
             {
+                "id": _id, 
                 "claim": claim,
                 "label": label,
                 "evidences": clean_evidences,
@@ -59,14 +63,14 @@ def process_trainjsonl():
 
 
 class FeverDataset(Dataset):
-    def __init__(self):
+    def __init__(self, wiki_path=config['wiki_out'], train_path=config['train_data']):
         super().__init__()
-        self.wiki = process_wiki()
+        self.wiki = process_wiki(wiki_path)
         
-        train_data = process_trainjsonl()
-        self.process(train_data)
+        train_data = process_trainjsonl(train_path)
+        train_data = self.process(train_data)
         
-        self.label_map = {"SUPPORTS": 0, "REFUTES": 1}
+        self.label_map = {"SUPPORTS": 1, "REFUTES": 0}
         
         self.data = []
         for datum in train_data:
@@ -76,6 +80,7 @@ class FeverDataset(Dataset):
             
             self.data.append(
                 {
+                    "id": datum['id'],
                     "label": self.label_map[label],
                     "claim": datum["claim"],
                     "evidences": datum["evidences"],
@@ -89,19 +94,23 @@ class FeverDataset(Dataset):
         return self.data[idx]
 
     def process(self, data):
+        data = data.copy()
         for datum in data:
             evidences = datum["evidences"]
             process_evidences = []
-            for doc_id, sent_id in evidences:
-                sent = self.wiki[doc_id]['lines'][sent_id]
+            
+            for doc_id, sent_ids in evidences.items():
                 process_evidences.append(
                     {
                         "title": doc_id,
-                        "sentence": sent
+                        "document": self.wiki[doc_id]['lines'],
+                        "sent_idx": sent_ids
                     }
                 )
             datum["evidences"] = process_evidences
+        
+        return data
 
 if __name__ == "__main__":
     dataset = FeverDataset()
-    # print(dataset[0])
+    print(dataset[0])
