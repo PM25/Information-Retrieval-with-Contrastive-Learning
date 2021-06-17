7ae5e0cbc3b6d86fec8ba32050f2b5b57d5aca2c
Pin-Yen Huang <pyhuang97@gmail.com>
2021-06-17 20:54:53 +0800

    fix path.

--- main_yen.py
+++ main_yen.py
@@ -5,5 +5,5 @@ from dataset import FeverDataset
 with open("config.yaml", "r") as stream:
     config = yaml.safe_load(stream)
 
-dataset = FeverDataset(config['small_wiki'], config['train_data'])
+dataset = FeverDataset(config["small_wiki"], config["data_dir"])
 print(dataset[1])
