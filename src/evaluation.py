b904c1e509bcb8820decfef645d998ab0c0c4fc4
Chi-Chang Lee <r08922a27@csie.ntu.edu.tw>
2021-06-26 03:49:41 +0800

    update

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,9 +2,13 @@ from tqdm import tqdm
 from src.model import load_model
 from src.dataset import get_dataloader
 import numpy as np
+import _pickle as pk
 import random
 import torch
 import os
+from preprocessing.drqa.retriever.utils import load_sparse_csr, filter_ngram, hash
+from preprocessing.drqa import tokenizers
+from preprocessing.docs_sentence_extraction import sentence_extraction
 
 OOM_RETRY_LIMIT = 10
 
@@ -49,23 +53,64 @@ def evaluate(dataloader, model):
     loss_avg = loss_sum / n_sample
     return loss_avg
 
+
+def documents_filtering(claim, args, count_matrix, metadata, full_doc_dict, bigram_only=True):
+    tokenizer = tokenizers.get_class(metadata['tokenizer'])()
+    ngrams = metadata['ngram']
+    hash_size = metadata['hash_size']
+
+    tokens = tokenizer.tokenize(claim)
+    res = tokens.ngrams(n=ngrams, uncased=True,filter_fn=filter_ngram)
+    
+    if bigram_only:
+        res = [t for t in res if len(t.split()) > 1]
+
+    wids = [hash(w, hash_size) for w in res]
+
+    wids_unique, wids_cnt = np.unique(wids, return_counts=True)
+
+    term, idx = count_matrix[wids_unique].nonzero()
+    idx = np.unique(idx)
+    
+    docs = {}
+    for i in idx:
+        # doc_dict[1]: idx to doc_id
+        doc_id = metadata["doc_dict"][1][i]
+        try:
+            docs[doc_id] = full_doc_dict[doc_id]
+        except:
+            continue
+    return docs
+
+
 def predict(args):
     assert not args.ckpt is None
     _, model, _, _ = load_model(args.ckpt)
     model = model.to(args.device)
     fever_loader = get_dataloader(args, train=False)
+    
+    matrix, metadata = load_sparse_csr(args.config["dataset"]["tfidf"])
+    count_matrix, _ = load_sparse_csr(args.config["dataset"]["inverted_file"])
+    with open(args.config["dataset"]["full_docs_dict"], "rb") as f:
+        full_docs_dict = pk.load(f)
+    import time
     with torch.no_grad():
         for batch in tqdm(fever_loader, desc="Iteration"):
             claim = [data['claim'] for data in  batch]
-            # claim = [data['evidences'][0]['document'][1] for data in  batch]
-            evdn = [data['evidences'][0]['document'][1] for data in  batch]
-            print(claim[0])
-            print(evdn[0])
+            s = time.time()
+            docs = documents_filtering(claim[0], args, count_matrix, metadata, full_docs_dict, False)
+            e = time.time()
+            print(e-s)
+            print(len(docs))
+            # # claim = [data['evidences'][0]['document'][1] for data in  batch]
+            # evdn = [data['evidences'][0]['document'][1] for data in  batch]
+            # print(claim[0])
+            # print(evdn[0])
             
-            clm_vec = model.ctx2vec(claim, args.device)
-            evdn_vec = model.ctx2vec(evdn, args.device)
-            print((clm_vec * evdn_vec).sum(dim=-1).mean())
-            random.shuffle(evdn)
-            evdn_vec = model.ctx2vec(evdn, args.device)
-            print((clm_vec * evdn_vec).sum(dim=-1).mean())
-            print('----------------------------------')
+            # clm_vec = model.ctx2vec(claim, args.device)
+            # evdn_vec = model.ctx2vec(evdn, args.device)
+            # print((clm_vec * evdn_vec).sum(dim=-1).mean())
+            # random.shuffle(evdn)
+            # evdn_vec = model.ctx2vec(evdn, args.device)
+            # print((clm_vec * evdn_vec).sum(dim=-1).mean())
+            # print('----------------------------------')
